3a16d90f5d444704938e80e451e0e894ec60c1d2
Jon Rowe <hello@jonrowe.co.uk>
2024-11-23 13:31:18 +0000

    Use script to run rspec for rspec-core

--- script/predicate_functions.sh
+++ script/predicate_functions.sh
@@ -1,3 +1,13 @@
+function is_mri {
+  if ruby -e "exit(!defined?(RUBY_ENGINE) || RUBY_ENGINE == 'ruby')"; then
+    # RUBY_ENGINE only returns 'ruby' on MRI.
+    # MRI 1.8.7 lacks the constant but all other rubies have it (including JRuby in 1.8 mode)
+    return 0
+  else
+    return 1
+  fi;
+}
+
 function is_ruby_23_plus {
   if ruby -e "exit(RUBY_VERSION.to_f >= 2.3)"; then
     return 0
