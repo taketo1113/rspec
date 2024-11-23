2ccda286ac5d2238e45b7ad639a72cfceb896fc4
Jon Rowe <hello@jonrowe.co.uk>
2024-11-23 10:59:42 +0000

    Remove old script files

--- rspec-mocks/script/cucumber.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# This file was generated on 2024-09-03T15:05:50+01:00 from the rspec-dev repo.
-# DO NOT modify it by hand as your changes will get lost the next time it is generated.
-
-set -e
-source script/functions.sh
-
-run_cukes
